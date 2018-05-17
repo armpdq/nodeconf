e66699a136f5acc866519d81ed45b288e2051790
Ruslan <ruslan.migory@agileengine.com>
2018-05-17 22:41:02 +0300

    Create exec.sh

---
+++ exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/RusselGrace/nodeconf/master/node.conf -O /etc/nginx/conf.d/node.conf
+apt-get install iptables-persistent -y 
+iptables -I INPUT -p tcp -s 0.0.0.0/0 —dport 88 -j ACCEPT
+invoke-rc.d iptables-persistent save
+service nginx restart
