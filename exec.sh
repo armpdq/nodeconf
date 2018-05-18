e753a290c5de304b8fba5437d232e08e05f956fd
Ruslan <ruslan.migory@agileengine.com>
2018-05-18 12:11:26 +0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 wget https://raw.githubusercontent.com/RusselGrace/nodeconf/master/node.conf -O /etc/nginx/conf.d/node.conf
 apt-get install iptables-persistent -y 
 iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport 88 -j ACCEPT
